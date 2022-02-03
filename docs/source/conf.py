a0ff373eb46b1f7e32280a4721d7b1c2e0debd17
user_name <12120524+dummyindex@users.noreply.github.com>
2022-02-02 23:26:52 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath("../../"))
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-source_suffix = [".rst", ".ipynb"]
+source_suffix = [".rst"]
 latex_additional_files = ['lap.bib']
 
 master_doc = "index"
